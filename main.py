5c9348c9e4e254ee5960c4fba0808da08b30d731
Vivaan <69657903+Vivaan-S@users.noreply.github.com>
2021-06-30 01:01:06 +0000

    Added readme and stuff

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from os import environ as env
-import flask
+try:
+	import flask
+	import os, sys
 
-app = flask.Flask(__name__)
+	app = flask.Flask(__name__)
 
-secret_ip = env["IP"]
+	secret_ip = os.environ["IP"]
 
-@app.route("/")
-def index():
-	return flask.render_template("index.html"), 200
+	@app.route("/")
+	def index():
+		return flask.render_template("index.html"), 200
 
-@app.route("/auth")
-def auth():
-	ip_address = flask.request.remote_addr
-	if ip_address == secret_ip:
-		return flask.render_template("success.html"), 200
-	else:
-		return "403 Unauthorized", 403
+	@app.route("/auth")
+	def auth():
+		ip_address = flask.request.remote_addr
+		if ip_address == secret_ip:
+			return flask.render_template("success.html"), 200
+		else:
+			return "403 Unauthorized", 403
 
-app.run(host="0.0.0.0", port=8080)
+	app.run(host="0.0.0.0", port=8080)
+except:
+	import os, sys
+	os.execv(sys.executable, ['python'] + sys.argv)
